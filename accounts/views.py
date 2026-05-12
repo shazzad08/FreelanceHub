25682c01c73a81b57d13150840da438ffe0f7185
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-05-13 00:36:12 +0600

    add logout and login feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
-
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 
 def register_view(request):
 
@@ -21,3 +22,13 @@ def register_view(request):
     }
 
     return render(request, 'accounts/register.html', context)
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+class UserLogoutView(LogoutView):
+
+    next_page = reverse_lazy('home')
