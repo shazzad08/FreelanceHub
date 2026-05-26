19a20712821df306b9fab42db4cab231d171c729
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-05-26 23:56:05 +0600

    implement profile filtering: search,category filter,budget sorting

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
 
     path('create/',views.create_project,name='create_project'),
     path('',views.project_list,name='project_list'),
-    path('<int:id>/',views.project_details,name='project_details')
+    path('<int:id>/',views.project_details,name='project_details'),
+    path('<int:id>/edit/', views.edit_project, name='edit_project'),
+    path('<int:id>/delete/',views.delete_project,name='delete_project'),
+    path('profile/<int:id>/',views.client_profile,name='client_profile'),
 
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .forms import ProjectForm
 from .models import Project
+from categories.models import Category
+from profiles.models import ClientProfile
+from accounts.models import User
 from django.shortcuts import get_object_or_404
 
 @login_required
@@ -31,9 +35,53 @@ def create_project(request):
 
 def project_list(request):
     
-    projects= Project.objects.all()
+    categories = Category.objects.all()
 
-    return render(request,'projects/project_list.html',{'projects': projects})
+    
+    if request.user.role == 'freelancer':
+
+        projects = Project.objects.all()
+
+    else:
+
+        projects = Project.objects.filter(
+            client=request.user
+        )
+
+    # Search
+    query = request.GET.get('q')
+
+    if query:
+
+        projects = projects.filter(
+            title__icontains=query
+        )
+
+    # Category Filter
+    category_id = request.GET.get('category')
+
+    if category_id:
+
+        projects = projects.filter(
+            category_id=category_id
+        )
+
+    # Budget Sort
+    sort = request.GET.get('sort')
+
+    if sort == 'low':
+
+        projects = projects.order_by(
+            'budget'
+        )
+
+    elif sort == 'high':
+
+        projects = projects.order_by(
+            '-budget'
+        )
+
+    return render(request,'projects/project_list.html',{'projects': projects,'categories': categories})
 
 
 
@@ -42,3 +90,86 @@ def project_details(request,id):
     project= get_object_or_404(Project,id=id)
 
     return render(request,'projects/project_details.html',{'project': project})
+
+
+
+@login_required
+def edit_project(request, id):
+
+    project = get_object_or_404(
+        Project,
+        id=id,
+        client=request.user
+    )
+
+    if request.method == 'POST':
+
+        form = ProjectForm(
+            request.POST,
+            instance=project
+        )
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('project_list')
+
+    else:
+
+        form = ProjectForm(instance=project)
+
+    return render(
+        request,
+        'projects/edit_project.html',
+        {'form': form,
+        'project': project}
+    )
+
+
+
+
+@login_required
+def delete_project(request, id):
+
+    project = get_object_or_404(
+        Project,
+        id=id,
+        client=request.user
+    )
+
+    if request.method == 'POST':
+
+        project.delete()
+
+        return redirect('project_list')
+
+    return redirect(
+        'project_details',
+        id=project.id
+    )
+
+
+
+
+def client_profile(request, id):
+    # Check if user exists
+    user = get_object_or_404(User, id=id)
+    
+    # Check if user is a client
+    if user.role != 'client':
+        raise Http404(f"User is a {user.role}, not a client.")
+    
+    # Get client profile
+    profile = get_object_or_404(
+        ClientProfile,
+        user__id=id
+    )
+
+    return render(
+        request,
+        'profiles/client_profile.html',
+        {
+            'profile_user': profile
+        }
+    )
