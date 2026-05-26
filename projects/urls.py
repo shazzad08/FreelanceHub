16e81c9fa7a0fc6c55b5aeedc7c3a7c87e23ce83
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-05-26 13:08:55 +0600

    built project listing and project details feature

--- projects/urls.py
+++ projects/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
 
     path('create/',views.create_project,name='create_project'),
+    path('',views.project_list,name='project_list'),
+    path('<int:id>/',views.project_details,name='project_details')
+
 ]
--- projects/views.py
+++ projects/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import ProjectForm
-
+from .models import Project
+from django.shortcuts import get_object_or_404
 
 @login_required
 def create_project(request):
@@ -27,3 +28,17 @@ def create_project(request):
 
     return render( request,'projects/create_project.html',{'form': form})
 
+
+def project_list(request):
+    
+    projects= Project.objects.all()
+
+    return render(request,'projects/project_list.html',{'projects': projects})
+
+
+
+def project_details(request,id):
+
+    project= get_object_or_404(Project,id=id)
+
+    return render(request,'projects/project_details.html',{'project': project})
