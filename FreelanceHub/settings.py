d427eeb40fd21e6ba6575da286f374422814f184
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-07-03 09:54:05 +0600

    Configure Cloudinary storage for Django 5.x

--- FreelanceHub/settings.py
+++ FreelanceHub/settings.py
@@ -53,7 +53,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
