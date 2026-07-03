4ba372a99bbdc8bb921e351c8ebc83ac3cc0cf56
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-07-03 17:38:07 +0600

    Fix broken profile migrations

--- profiles/migrations/0009_clientprofile_cover_image_and_more.py
+++ profiles/migrations/0009_clientprofile_cover_image_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('profiles', '0008_remove_clientprofile_city_and_more'),
+    ('profiles', '0006_remove_clientprofile_location_and_more'),
     ]
 
     operations = [
