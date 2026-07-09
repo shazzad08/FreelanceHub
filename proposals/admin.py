dfebeeec9bba2afd40a3648e0000a4886cb541d4
Shazzadul Islam Chowdhury <sazzadchowdhury87@gmail.com>
2026-07-09 13:00:54 +0600

    change proposal admin dashboard

--- proposals/admin.py
+++ proposals/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import Proposal
 # Register your models here.
-admin.site.register(Proposal)
+
+@admin.register(Proposal)
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "project_title",
+        "freelancer_email",
+        "bid_amount",
+        "status",
+        "created_at",
+    )
+
+    @admin.display(description="Project Title")
+    def project_title(self, obj):
+        return obj.project.title
+
+    @admin.display(description="Freelancer Email")
+    def freelancer_email(self, obj):
+        return obj.freelancer.email
